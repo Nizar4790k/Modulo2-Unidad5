247937cdcce9bd230396d2844550172b021d55d3
Nizar Sanchez <nizar4790k@gmail.com>
2021-09-14 17:39:11 -0400

    El modelo fue creado

---
+++ Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea5.Models
+{
+    public class Empleado
+    {
+        public int Codigo { get; set; }
+        public string Nombre {get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public double SueldoNeto { get; set; }
+
+
+
+    }
+}
