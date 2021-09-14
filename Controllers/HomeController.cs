7356cad23c8d174949da0783061fc76861ae737e
Nizar Sanchez <nizar4790k@gmail.com>
2021-09-14 18:52:49 -0400

    the views are ready

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tarea5.Models;
 
 namespace Tarea5.Controllers
 {
@@ -10,8 +11,32 @@ namespace Tarea5.Controllers
     {
         public ActionResult Index()
         {
+
+           List <Empleado> listaEmpleados = new List<Empleado>();
+
+            return View(listaEmpleados);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo,Nombre,FechaIngreso,SueldoNeto")] Empleado empleado)
+        {
+           
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Create()
+        {
             return View();
         }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+       
 
         public ActionResult About()
         {
--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tarea5.Models
 {
     public class Empleado
     {
+        [Key]
         public int Codigo { get; set; }
+        [Required]
         public string Nombre {get; set; }
+        [Required]
         public DateTime FechaIngreso { get; set; }
+        [Required]
         public double SueldoNeto { get; set; }
 
 
