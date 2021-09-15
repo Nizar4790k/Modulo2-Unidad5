5a19a9ca2f2464823166d93e5d47fb32d3fbf469
Nizar Sanchez <nizar4790k@gmail.com>
2021-09-15 16:37:53 -0400

    edit part is now working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Tarea5.Models;
@@ -9,10 +10,24 @@ namespace Tarea5.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static List<Empleado> listaEmpleados;
+        private static int codigo=1;
+
         public ActionResult Index()
         {
 
-           List <Empleado> listaEmpleados = new List<Empleado>();
+            if (Session["listaEmpleados"] != null)
+            {
+                listaEmpleados = Session["listaEmpleados"] as List<Empleado>;
+            }
+            else
+            {
+                listaEmpleados = new List<Empleado>();
+                Session["listaEmpleados"] = listaEmpleados;
+                
+            }
+
 
             return View(listaEmpleados);
         }
@@ -20,11 +35,46 @@ namespace Tarea5.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Codigo,Nombre,FechaIngreso,SueldoNeto")] Empleado empleado)
         {
-           
 
-            return RedirectToAction("Index");
+            
+
+            if (ModelState.IsValid)
+            {
+                empleado.Codigo = codigo;
+                codigo++;
+                listaEmpleados = Session["listaEmpleados"] as List<Empleado>;
+                listaEmpleados.Add(empleado);
+                Session["listaEmpleados"] = listaEmpleados;
+                return RedirectToAction("Index");
+            }
+
+            return View(empleado);
+
+            
         }
 
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+          
+                var empleado = HomeController.listaEmpleados.Where(emp => emp.Codigo == id).First();
+
+                if (empleado == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(empleado);
+            
+            
+            return View(empleado);
+        }
 
         public ActionResult Create()
         {
